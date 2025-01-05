e1c931d2a6ac513b9dd8442df8700d2c79b66698
donaitorin <donai.torin@gmail.com>
2025-01-04 21:34:54 -0300

    adding footer

---
+++ src/components/atoms/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import { IMAGES_ASSETS_PATHS } from '@/utils/images';
+import React from 'react';
+
+export const Footer = () => {
+	return (
+		<footer className="bg-gray-footer flex justify-center items-center h-44 p-5">
+			<img src={IMAGES_ASSETS_PATHS.Logo} alt="apply digital logo" className="w-44" />
+		</footer>
+	);
+};
