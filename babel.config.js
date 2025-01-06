a1172dd94f1f47f59b96d979968b8210a0165df6
donaitorin <donai.torin@gmail.com>
2025-01-06 16:58:41 -0300

    fix build errors for deploy

--- babel.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	presets: [
-		'next/babel', // Use Next.js's Babel preset
-	],
-};
--- src/components/templates/CatalogPage/CatalogPage.tsx
+++ src/components/templates/CatalogPage/CatalogPage.tsx
@@ -4,15 +4,12 @@ import { PageTitle } from '@/components/atoms/PageTitle/PageTitle';
 import { Filters } from '@/components/molecules/Filters/Filters';
 import { GamesList } from '@/components/organisms/GamesList/GamesList';
 import { useFetchGames } from '@/hooks/useFetchGames';
-import { useRouter, useSearchParams } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 
 export const CatalogPage: React.FC = () => {
-	const router = useRouter();
-	const searchParams = useSearchParams();
 	const [params, setParams] = useState({
-		genre: searchParams.get('genre') || '',
-		page: searchParams.get('page') || '1',
+		genre: '',
+		page: '0',
 	});
 	const { data, loading, error, handleFetchGames, setData } = useFetchGames();
 
@@ -27,16 +24,6 @@ export const CatalogPage: React.FC = () => {
 			totalPages: 0,
 			currentPage: 0,
 		});
-		const params = new URLSearchParams(searchParams.toString());
-		if (newFilter) {
-			params.set('genre', newFilter);
-			params.delete('page');
-		} else {
-			params.delete('genre');
-			params.delete('page');
-		}
-
-		router.push(`?${params.toString()}`);
 	};
 
 	const handleSeeMore = () => {
