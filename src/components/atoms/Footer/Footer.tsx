9071132bdbac416be5b16e1a5f75f94824c12f3c
donaitorin <donai.torin@gmail.com>
2025-01-07 10:35:48 -0300

    adding redirect i footer icon

--- src/components/atoms/Footer/Footer.tsx
+++ src/components/atoms/Footer/Footer.tsx
@@ -1,10 +1,13 @@
 import { IMAGES_ASSETS_PATHS } from '@/utils/images';
+import Link from 'next/link';
 import React from 'react';
 
 export const Footer = () => {
 	return (
 		<footer className="bg-gray-footer flex justify-center items-center h-44 p-5">
-			<img src={IMAGES_ASSETS_PATHS.Logo} alt="apply digital logo" className="w-44" />
+			<Link href={'/'}>
+				<img src={IMAGES_ASSETS_PATHS.Logo} alt="apply digital logo" className="w-44" />
+			</Link>
 		</footer>
 	);
 };
